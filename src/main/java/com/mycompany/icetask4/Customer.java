704505b7562d3a081265f3b3666867a9fd4add67
ST10512564 <st10512564@myemeris.edu.za>
2026-04-09 11:25:30 +0200

    Commit 02: Adding Points
    Create method to add 10  points and notify user.

--- src/main/java/com/mycompany/icetask4/Customer.java
+++ src/main/java/com/mycompany/icetask4/Customer.java
@@ -12,14 +12,24 @@ public class Customer {
 
     public static void main(String[] args) {
        
+        //These must be at the very top of the class
+    private String name;
+    private int points;
 
-    // Const
+    // Constructor to initialize name and set points to 0 [cite: 47]
+    public Customer(String name) {
+        this.name = name;
+        this.points = 0;
+    }
+ // Adds 10 points and notifies the user [cite: 10, 49]
+    public void addPoints() {
+        this.points += 10;
+        System.out.println("10 Points added");
+    }
 
    
 }
-        ructor to initialize name and set points to 0 [cite: 47]
-    public Customer(String name) {
-    }
+        
         
         
     }
