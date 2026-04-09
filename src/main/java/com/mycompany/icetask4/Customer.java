f48eea420c9f1007a621857c3c38a018b3b375bc
ST10512564 <st10512564@myemeris.edu.za>
2026-04-09 11:21:07 +0200

    Commit 01: Core Properties
    Define name and points variables and initialize them via the constructor.

---
+++ src/main/java/com/mycompany/icetask4/Customer.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask4;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Customer {
+
+    public static void main(String[] args) {
+       
+
+    // Const
+
+   
+}
+        ructor to initialize name and set points to 0 [cite: 47]
+    public Customer(String name) {
+    }
+        
+        
+    }
+}
