3dd27c5a68a69b26bac22f3646ff89dc02d9dde6
ST10512564 <st10512564@myemeris.edu.za>
2026-04-09 11:43:18 +0200

    Commit 05: Redemption Logic
    Implement Menu

--- src/main/java/com/mycompany/icetask4/BeanCafe.java
+++ src/main/java/com/mycompany/icetask4/BeanCafe.java
@@ -15,5 +15,12 @@ public class BeanCafe {
         Scanner input = new Scanner(System.in);
         Customer user = new Customer("Taelo"); 
         int choice = 0;
-    
+       while (choice != 4) {
+            // Display Menu [cite: 4-8]
+            System.out.println("\nJava Bean Café ---");
+            System.out.println("1. Buy Coffee (+10 pts)");
+            System.out.println("2. Redeem Reward (50 pts)");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+            System.out.print("Enter choice: ");
 }
